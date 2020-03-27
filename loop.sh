7dbdef5eaf1b9ee1dc8e3f81eb93915138fff60b
PeronTheDuck <pedroignacioperezvargas@gmail.com>
2020-03-26 21:34:23 -0300

    Add comments

--- loop.sh
+++ loop.sh
@@ -2,14 +2,16 @@
 
 lastUrl=""
 
+# DBus connection data
 sender=org.mpris.MediaPlayer2.spotify 
 interface=org.freedesktop.DBus.Properties
 member=PropertiesChanged
+# Listen to Spotify's DBus. Pipe signals to the while loop
 dbus-monitor --profile "interface='$interface',member='$member',type='signal',sender='$sender'" | 
 while read -r line; do
 	newUrl=$(./get-album-art-url)
 	if [[ $newUrl = $oldUrl ]]; then
-		echo Not updating
+		#echo Not updating
 	else
 		oldUrl=$newUrl
 		./update.sh
---
+++ wal-cava.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+original=$HOME/.config/cava/config
+copy=$HOME/.config/cava/walconfig
+
+if [ "$1"="-h" ]; then
+	echo "$0 updates '$copy' color setting to use a gradient with the current pywal colors"
+	echo "Options: "
+	echo "$0 <end>			# Take from 0 to <end> colors from pywal"
+	echo "$0 <start> <end>	# Take from <start> to <end>"
+	exit 1
+fi
+
+# You can remap pywal colors
+if [ $# -eq 0 ]; then
+	# Pywal colors [0; 7] will be used
+	start=0
+	stop=7
+elif [ $# -eq 1 ]; then
+	# Pywal colors [0; $1] will be used
+	start=0
+	stop=$1
+elif [ $# -eq 2 ]; then
+	# Pywal colors [$1; $2] will be used
+	start=$1
+	stop=$2
+fi
+
+. "$HOME/.cache/wal/colors.sh"
+if [ ! -f "$HOME/.config/cava/walconfig" ];
+	# I don't trust my code, so just to be safe, use "walconfig" instead of the default "config" file
+	cp $original $copy
+fi
+
+touch dyn
+echo 'gradient = 1' >> dyn
+printf "gradient_count = %s\n" "$(( $stop - $start))" >> dyn
+for c in $(seq $start $stop); do
+	color="color$c"
+	printf "gradient_color_%s = '%s'\n" "$(($c - $start))" ${!color} >> dyn
+done		
+sed '/^gradient/d ; /\[color\]/rdyn' -i $copy
+rm dyn
